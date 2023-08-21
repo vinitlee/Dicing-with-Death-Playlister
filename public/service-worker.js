0cad0c2fb23246c128fe0388f89c4f449505149d
Vincent Lee <vincent_lee@bose.com>
2023-08-21 01:09:31 -0700

    Barebones functional version, deployed Has PWA functionality and placeholder icon

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            // return cache.addAll([
+            // '/',
+            // '/index.html',
+            // '/styles.css',
+            // '/dwd_data.js',
+            // '/icon.png'
+            // ]);
+            return cache.addAll(['/']);
+        })
+        );
+    });
+    
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+            );
+        });
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Dicing with Death Playlister
+
+I got much too angry at scrolling and waiting for youtube to load my playlist so here's a stupidly dumb tool to address that.
+
+## Current Status
+
+- Updated to Episode 220
+- Not styled in the least
+- Saves current episode on device
+- Links to wiki
